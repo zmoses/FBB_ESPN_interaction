a828e978189696fd835e4c0ca9e13533df0a3d6e
Zach Moses <zmoses@Zachs-MacBook-Air.local>
2024-08-19 11:48:48 -0700

    this actually works, poc approved

--- lambda_function.rb
+++ lambda_function.rb
@@ -2,13 +2,13 @@ require 'json'
 
 def lambda_handler(event:, context:)
   url = "https://lm-api-reads.fantasy.espn.com/apis/v3/games/flb/seasons/2024/segments/0/leagues/#{ENV['SWID']}?scoringPeriodId=151&view=kona_playercard"
-  headers = {'Cookie' => "espn_s2=#{ENV['ESPN_S2']}", 'X-Fantasy-Filter' => '{"players":{"filterIds":{"value":[4781491]},"filterStatsForTopScoringPeriodIds":{"value":162,"additionalValue":["002024","102024","002023","012024","022024","032024","042024","062024","010002024"]}}}'}
+  headers = {
+    'Host' => 'lm-api-reads.fantasy.espn.com',
+    'Cookie' => "espn_s2=#{ENV['ESPN_S2']}",
+    'X-Fantasy-Filter' => '{"players":{"filterIds":{"value":[4781491]},"filterStatsForTopScoringPeriodIds":{"value":162,"additionalValue":["002024","102024","002023","012024","022024","032024","042024","062024","010002024"]}}}'
+  }
 
-  response = Net::HTTP.post(URI(url), {}.to_json, headers)
+  response = JSON.parse(Net::HTTP.get(URI(url), headers))
   
-  if response.is_a?(Net::HTTPSuccess)
-    { statusCode: 200, body: response.body }
-  else
-    { statusCode: 500 }
-  end
+  { statusCode: 200, body: response.dig('players') || {} }
 end
